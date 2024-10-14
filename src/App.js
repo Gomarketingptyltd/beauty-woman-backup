bc900de23f30492d8b652b82de228fd9b090437a
WeidongFang1985 <fangweidong1985@gmail.com>
2024-10-14 20:54:55 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from "./components/Header/Header";
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React, {useEffect, useRef} from 'react';
+import './Header.css';
+
+const Header = () => {
+	const videoRef = useRef(null);
+	useEffect(() => {
+		if (videoRef.current) {
+			videoRef.current.playbackRate = 0.9;
+		}
+	}, []);
+
+  return (
+		<div>
+			<video autoPlay muted loop id="bg-video" ref={videoRef}>
+				<source src={`${process.env.PUBLIC_URL}/video.mp4`} type="video/mp4"/>
+				Your browser does not support the video tag.
+			</video>
+			<div className="content-overlay">
+				<h1>Pussy Cats</h1>
+				<p>No.1 Asian Brothel in Melbourne</p>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
