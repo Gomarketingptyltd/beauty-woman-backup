11fb25aaeaa17becbb006c6f51a7462a3c69e0f1
WeidongFang1985 <fangweidong1985@gmail.com>
2024-10-24 12:06:06 +0800

    About done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,21 +15,19 @@ const About = () => {
 	};
 
 	const reasons = [
-		{ id: 1, title: "Best Service Guaranteed", reason: "We pride ourselves on the quality of our service, and are well known for having beautiful yet very friendly girls. Indulge your wildest fantasies with one... two or more of our lovely ladies. They offer a service like no other and best of all, they love what they do. When you arrive you will be greeted by one of our friendly receptionists who can help you find the right lady for you." },
+		{ id: 1, title: "Best Service Guaranteed", reason: "We pride ourselves on the quality of our service, and are well known for having beautiful yet very friendly girls. Indulge your wildest fantasies with one... two or more of our lovely ladies. " },
 		{ id: 2, title: "Everyday New Faces Ready", reason: "We constantly update our lineup to ensure you have new, exciting experiences." },
-		{ id: 3, title: "The best ladies Melbourne has to offer", reason: "We select only the finest and friendliest ladies for our clientele." }
+		{ id: 3, title: "The best ladies Melbourne ", reason: "We select only the finest and friendliest ladies for our clientele." }
 	];
 
 	return (
 		<div className="about">
 			<h1>Scarlet Lady Asian Brothel and Escort</h1>
-			<h3>A new-style gentlemen's parlour in Melbourne</h3>
+			<h3>A new-style gentleman's parlour in Melbourne</h3>
 			<div className="about__container">
-				{/* 左边是图片 */}
 				<div className="about__image-container">
 					<img src={sexy} alt="Scarlet Lady" className="about__image" />
 				</div>
-				{/* 右边是文本 */}
 				<div className="about__content">
 					<h2>Why Choose Us?</h2>
 					<div className="reasons">
@@ -41,10 +39,11 @@ const About = () => {
 									</div>
 									<h3 className="reason__title">{item.title}</h3>
 								</div>
-								{/* 当用户点击时，显示 reason，默认隐藏 */}
-								{expanded[item.id] && (
+								<div
+									className={`reason__description-container ${expanded[item.id] ? 'expanded' : ''}`}
+								>
 									<p className="reason__description">{item.reason}</p>
-								)}
+								</div>
 							</div>
 						))}
 					</div>
